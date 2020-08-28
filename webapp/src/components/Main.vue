9cf135d94143b08306d8d1912135a065cb440c92
Mark Havryliv <mark@scoreaddiction.com>
2020-08-28 11:51:41 +1000

    generating time labels

--- webapp/src/components/Main.vue
+++ webapp/src/components/Main.vue
@@ -34,6 +34,18 @@
     </div>
 
     <hr>
+      <div class="historic_data_wrapper">
+        <h3>Past 24 hours</h3>
+        <button @click="reloadHistoricData">Reload</button>
+
+        <div class="small">
+          <line-chart :chart-data="datacollection" :options="chartOptions" :styles="myStyles"
+          ></line-chart>
+
+        </div>
+      </div>
+
+    <hr>
 
     <button @click="getCurrentData">Refresh</button>
 
@@ -47,11 +59,46 @@ var axios = axiosLib.create({
 });
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
+import LineChart from './LineChart.js'
+
+
 export default {
   name: 'Main',
+  components: {
+    LineChart
+  },
   data() {
     return {
-      currentWeather: {loading: false}
+      currentWeather: {loading: false},
+      historicData: {
+        loading: false,
+        count: 0,
+        events: []
+      },
+      datacollection: {},
+      chartOptions: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              display: true
+            }
+          }],
+          xAxes: [ {
+            gridLines: {
+              display: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        responsive: true,
+        maintainAspectRatio: false
+      },
+      chartHeight: 300
     }
   },
   computed: {
@@ -63,20 +110,62 @@ export default {
         hour: 'numeric', minute: 'numeric', second: 'numeric'};
         return date.toLocaleDateString('en-AU', options);
       }
+    },
+    myStyles () {
+      return {
+        // height: this.chartHeight + 'px',
+        // position: 'relative'
+      }
+    },
+    historicDataTimeRange() {
+      if(this.historicData.events.length < 2) {
+        return undefined;
+      }
+      return {
+        start: this.historicData.events[0].time,
+        end: this.historicData.events[this.historicData.count - 1].time
+      }
     }
   },
   methods: {
+    async reloadHistoricData() {
+      if(this.historicData.loading) {
+        console.log("Already loading data");
+        return;
+      }
+      this.historicData.loading = true;
+      const data = await this.getEventHistory(86400);
+      this.historicData.loading = false;
+      if(!data) {
+        console.log("No historic data!");
+        this.historicData.count = 0;
+        this.historicData.events = [];
+        return;
+      }
+      this.historicData = data;
+      console.log(this.historicData);
+      const timerange = this.historicDataTimeRange;
+      const start = new Date(timerange.start);
+      const end = new Date(timerange.end);
+      console.log(start);
+      console.log(end);
+      this.fillTempData();
+
+
+    },
     async getEventHistory(numSecBack) {
       const postData = {
-        getPastSeconds: 86400
+        getPastSeconds: numSecBack
       }
       const data = await this.doPostCall(postData);
       if(!data) {
         return console.log("Couldn't get event history");
       }
+      return data;
       
     },
     async getCurrentData() {
+      this.currentWeather.loading = true;
       const postData = {
         getCurrent: true
       }
@@ -87,10 +176,9 @@ export default {
       }
       const event = data.events[0];
       this.currentWeather = event;
-
+      console.log("Got something for: " + new Date(event.time));
     },
     async doPostCall(postData) {
-      this.currentWeather.loading = true;
       try {
         const res = await axios.post('', postData);
         const data = res.data;
@@ -100,10 +188,71 @@ export default {
         console.log("Error requesting data " + e);
         return Promise.resolve();
       }
+    },
+    generateDateLabels(startTime, endTime, numToMake) {
+      let labels = [];
+      var options = {hour: 'numeric', minute: 'numeric'};
+      const interval = Math.round((endTime - startTime) / numToMake);
+      // let numToMake = Math.round((endTime - startTime) / interval);
+      for(var i = 0; i < numToMake; ++i) {
+        if(i === (numToMake - 1)) {
+          options.minute = 'numeric'
+        }
+        else {
+          options.minute = undefined;
+        }
+        const date = new Date(startTime + (interval * i));
+        const dateStr = date.toLocaleTimeString('en-AU', options);
+        labels.push(dateStr);
+      }
+
+      return labels;
+    },
+    fillTempData() {
+      const timerange = this.historicDataTimeRange;
+      if(!timerange) {
+        return console.log("No time range to fill temperature data!");
+      }
+      const hourInMsec = 60 * 60 * 1000;
+      const labels = this.generateDateLabels(timerange.start, timerange.end, 24);
+      console.log(labels);
+      this.datacollection = {
+        labels: [1, 2, 3, 4, 5, 6],
+        datasets: [
+          {
+            label: "Temp (c)",
+            backgroundColor: 'rgba(0, 50, 100, 0.5)',
+            data: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+          }
+        ]
+      }
+    },
+    fillData () {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }, {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }
+        ]
+      }
+    },
+    getRandomInt () {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    },
+    increase () {
+     this.chartHeight += 10
     }
   },
   mounted() {
     this.getCurrentData();
+    this.fillTempData();
   }
 
 }
@@ -128,5 +277,10 @@ export default {
       }
     }
   }
+}
+
+.small {
+  max-width: 600px;
+  margin:  150px auto;
 }
 </style>
